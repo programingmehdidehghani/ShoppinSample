9aa51275bdf04aa36953752936027565a36d69af
mehdi <dehghani_m@rocketmail.com>
2023-04-28 16:37:15 +0430

    continue fragment

--- app/src/main/java/com/example/testshoppingmarket/ui/MainActivity.kt
+++ app/src/main/java/com/example/testshoppingmarket/ui/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.testshoppingmarket.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.testshoppingmarket.R
+import com.example.testshoppingmarket.databinding.ActivityMain2Binding
+import com.example.testshoppingmarket.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main2.bottomNavigationView
+import kotlinx.android.synthetic.main.activity_main2.shopNavHostFragment
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        bottomNavigationView.setupWithNavController(shopNavHostFragment.findNavController())
+
     }
 }
