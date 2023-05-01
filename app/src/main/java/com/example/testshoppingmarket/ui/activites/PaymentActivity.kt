e66eeebf94a8f1fb132e4e883f13d1c4bd9fac1b
mehdi <dehghani_m@rocketmail.com>
2023-05-01 07:00:05 +0430

    start make payment

---
+++ app/src/main/java/com/example/testshoppingmarket/ui/activites/PaymentActivity.kt
@@ -0,0 +1,12 @@
+package com.example.testshoppingmarket.ui.activites
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.testshoppingmarket.R
+
+class PaymentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_activity_payment)
+    }
+}
